e67c6ddbaaab851d874575432330b2925bea6cfa
JLWolff <joaolwolff@gmail.com>
2020-05-21 22:42:08 +0100

    worked?

---
+++ build/precache-manifest.ca3381fc0e5d2acafa7b6c83cdf46ce0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d3a606ec5560e31913f450113a6322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "438db69b7fc35b2378f3",
+    "url": "/static/css/main.b8004405.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438db69b7fc35b2378f3",
+    "url": "/static/js/main.e6b73089.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "2697ffb623297000b3fbb7c2a05aa418",
+    "url": "/static/media/Night Club 70s Bold.2697ffb6.woff"
+  }
+]);
