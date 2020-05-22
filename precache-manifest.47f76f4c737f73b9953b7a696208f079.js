5760bbfdc20982a78027f7771f4e8ab8b5da817d
JLWolff <joaolwolff@gmail.com>
2020-05-22 08:58:03 +0100

    Updates

---
+++ precache-manifest.47f76f4c737f73b9953b7a696208f079.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789e95f02b660026c7655d738841012f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "97ba8032066ff45048b9",
+    "url": "/robofriends/static/css/main.96912e53.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ba8032066ff45048b9",
+    "url": "/robofriends/static/js/main.6e38f464.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "2697ffb623297000b3fbb7c2a05aa418",
+    "url": "/robofriends/static/media/Night Club 70s Bold.2697ffb6.woff"
+  }
+]);
